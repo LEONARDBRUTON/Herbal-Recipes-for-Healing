5c4b1718324f4989b5a208ebc39ba5a325251ac4
enigmalb <enigmalb@outlook.com>
2020-06-10 19:30:12 -0700

    added seed file and deleted some files

---
+++ db/seeds.rb
@@ -0,0 +1,73 @@
+User.create(username: "Tammy",password: "cc")
+User.create(username: "Aubrey",password: 'honeybee')
+
+
+HerbalRecipe.create(title: "Digestive, and Detox Issues" , herb: "Cilantro (Coriandrum sativum)  Cilantro boasts a unique flavor that people either love or hate. The leaves often garnish Mexican and Thai dishes. The seeds, known as coriander, are a prime ingredient in Indian curries.
+
+Few think of this plant as a medicinal herb, but researchTrusted Source shows it’s a powerful digestive aid and may be capable of removing heavy metals and other toxic agents from the body.
+
+Cilantro grows best in a cool, moist garden and will quickly bolt in hot weather. Look for slow bolt varieties from seed companies. Try this recipe for cleansing cilantro pesto. " ,ingredents: "Ingredients
+1 cup Coriander leaves
+1 Cucumber
+1 Lemon
+1/2 inch Ginger
+to taste Salt" ,instructions: "Steps
+Take cucumber, Ginger & coriander in a grinder.
+
+Add salt & lemon juice in it.
+
+Add some water in it. Blend properly.
+
+Strain it. It's ready." ,user_id:1)
+
+
+HerbalRecipe.create(title: "What does goldenseal treat?
+gonorrhea.
+malaria.
+pneumonia.
+just about any stomach or digestive condition.
+skin problems such as dandruff, ringworm, and eczema.
+eye infections.
+bladder infections." ,herb: "Goldenseal   is a plant that grows in North America. It has been used to help with digestion and treat colds. The root is dried and can be made into a tea. It can also be taken as a pill, powder, or extract." ,ingredents: "Goldenseal, Lemon, Honey" ,instructions: "Procedure:
+Boil the water in a kettle.
+Put 1 teaspoon of goldenseal powdered root or leaves in a teacup.
+Pour the boiling water over the goldenseal and allow to steep for 15-20 minutes.
+Strain the tea and serve hot. Add lemon or raw organic honey, to taste." ,user_id:1)
+
+
+HerbalRecipe.create(title: "Immune Boosters-Elderberry Syrup with Echinacea and Goldenseal" ,ingredents: "Ingredients
+1 cup dried elderberries
+¼ cup dried echinacea root
+1 teaspoon goldenseal root powder
+2 piece ginger
+2 cinnamon sticks
+6 whole cloves
+1 cup raw honey
+4 cups water" ,instructions: "Instructions
+Put the water, elderberries, and all of the herbs and spices into a pot and bring it to a rolling boil. 
+Turn down heat just a bit and let it boil for a good 45 minutes to 1 hour. 
+Remove from the heat, let cool down to lukewarm, and strain out the liquid from the berries and herbs.
+Add the honey to lukewarm liquid, as honey also has healing properties that are killed with heat. Stir the honey until it is dissolved. 
+Bottle and store in the refrigerator. 
+Notes
+You can substitute 2 cups of fresh elderberries for the dried ones. 
+Possible substitutions for goldenseal would be barberry or Oregon grape.
+Omit goldenseal (and substitutes) if this syrup will be given to children or pregnant women. 
+Do not take for more than 1-2 weeks in a row. 
+Standard Syrup Dosage: 1 tablespoon every 4 hours for adults
+Nutrition
+Serving: 1Tablespoon | Calories: 14kcal" ,user_id:2)
+
+
+HerbalRecipe.create(title: "DIY Coconut Brown Sugar Scrub" ,ingredents: "Ingredients
+1 cup  brown sugar
+2 tbsp  fractionated coconut oil
+5-10 drops essential oils" ,instructions: "Instructions
+
+Add brown sugar to glass bowl.
+Add coconut oil and essential oils (optional) and stir gently.
+Spoon into a Mason jar for storage.
+Recipe Notes
+How to Use This DIY Coconut Brown Sugar Scrub
+For Body: Now for the fun part! When taking a bath or shower, simply scoop out the desired amount of scrub with a small spoonor spatula and exfoliate from neck to feet. Concentrate on especially dry areas like elbows and knees for really soft moisturized skin.
+For Hands: I love to use this scrub after washing my hands. All you have to do is scoop out a small amount and massage your hands, especially around the cuticles." ,user_id:2)
